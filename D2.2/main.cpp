c4281da6dd2c51c5d0f68dcdb9c478e124eefe8f
MadGeorge <georgeneversleep@gmail.com>
2022-11-30 15:47:18 +0400

    T2

--- D2.2/main.cpp
+++ D2.2/main.cpp
@@ -8,10 +8,42 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+// infinitely recursive
+int getInput() {
+    fflush(stdin);
+
+    float a,b;
+
+    printf("You input is: ");
+
+    if (scanf("%f %f", &a, &b) == 2) {
+        printf("Comparing: %f > %f\n", a, b);
+
+        if (a == b) {
+            printf("Numbers are EQUALS\n");
+        } else if (a > b) {
+            printf("First number is GREATER\n");
+        } else {
+            printf("First number is SMALLER\n");
+        }
+
+        printf("\nInput next pair of numbers or press CNTRL+C to exit\n");
+
+        return getInput();
+    }
+
+    printf("Invalid input. Please, try again.\n");
+
+    return getInput();
+}
 
 int main(int argc, const char * argv[]) {
+    printf("Input two numbers separated by the space.\n");
+    printf("Use dot as decimal separator.\n");
+    printf("Example input: '9.4 54.034' and not '9,4 54,034'\n");
 
-    printf("D2.2\n");
-
-    return 0;
+    return getInput();
 }
